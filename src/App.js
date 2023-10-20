ad55b0845d19c45d10b575ea17072b85ceccef22
Learnuel1 <learnueltech@gmail.com>
2023-10-20 20:31:10 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {io} from "socket.io-client";
+import { useEffect } from 'react';
+import { useState } from 'react';
 function App() {
+  const [location, setLocation] = useState("");
+  const socket = io();
+  const user ={userId: "87hjfgr453228", receiver: "12sf7834uioioi53424"};
+  socket.emit("lock", user);
+
+  socket.on("notify", payload =>{
+    setLocation(payload)
+    console.log("location received");
+    console.log(payload);
+  })
+  
+  const sendLocation = () => {
+    user.location = "abj";
+    console.log(user)
+  socket.emit("location", user);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     
+     <input> </input>
+         
+        <button className="btn"  onClick ={(e) =>sendLocation()}>
+              send location2
+        </button>
+    </>
   );
 }
 
